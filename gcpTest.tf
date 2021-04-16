a0d8d1f4d9df40ec8dd7d61643a322c4129fcdca
relliott-harness <78382621+relliott-harness@users.noreply.github.com>
2021-04-16 09:03:10 -0500

    Update gcpTest.tf

--- gcpTest.tf
+++ gcpTest.tf
@@ -1,49 +1,29 @@
 provider "google" {
-  version = "3.5.0"
-
-  credentials = var.creds
-  project = "sales"
+  credentials = var.gcp_creds
+  project = "Sales"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
-variable "hostname" {
-  default = "robin-tf-001"
+# Harness parameterized set variables
+variable "gcp_creds" {
+  type = string
 }
-variable "creds" {
-}
-
-#terraform {
-#  backend "gcs" {
-#    bucket      = "gcp-global-bucket"
-#    prefix      = "terraform"
-#    credentials = file("carvana-harness-test-7c0871fbf281.json")
-#  }
-#}
-
-#resource "google_compute_attached_disk" "default" {
-#  disk     = google_compute_disk.default.id
-#  instance = google_compute_instance.default.id
-#}
 
 resource "google_compute_instance" "vm_instance" {
-  name         = var.hostname
-  machine_type = "g1-small"
+  name         = "robin-tf-001"
+  machine_type = "f1-micro"
 
   boot_disk {
     initialize_params {
-      image = "centos-7-v20210401"
+      image = "debian-cloud/debian-9"
     }
   }
 
   network_interface {
+    # A default network is created for all GCP projects
     network = "default"
     access_config {
     }
   }
 }
-
-
-#output "ip" {
-#  value = google_compute_instance.google_compute_address.address
-#}
