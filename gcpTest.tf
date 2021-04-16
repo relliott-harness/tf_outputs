886570bc29ed93149e2f05ab7e8a4ae6ee413c46
relliott-harness <78382621+relliott-harness@users.noreply.github.com>
2021-04-16 08:28:53 -0500

    Create gcpTest.tf

---
+++ gcpTest.tf
@@ -0,0 +1,49 @@
+provider "google" {
+  version = "3.5.0"
+
+  credentials = var.creds
+  project = "sales"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+variable "hostname" {
+  default = "robin-tf-001"
+}
+variable "creds" {
+}
+
+#terraform {
+#  backend "gcs" {
+#    bucket      = "gcp-global-bucket"
+#    prefix      = "terraform"
+#    credentials = file("carvana-harness-test-7c0871fbf281.json")
+#  }
+#}
+
+#resource "google_compute_attached_disk" "default" {
+#  disk     = google_compute_disk.default.id
+#  instance = google_compute_instance.default.id
+#}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.hostname
+  machine_type = "g1-small"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-7-v20210401"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+
+output "ip" {
+  value = google_compute_instance.ip_address
+}
